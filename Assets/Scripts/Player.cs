50f368587d37f753971a1f714c041a17d73c85e0
Deseteral <deseteral@gmail.com>
2020-04-18 12:51:43 +0200

    Player controller look the right way

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,19 @@ public class Player : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
     }
 
+    void Update() 
+    {
+        // TODO: Controller support
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000, (1 << 8)))
+        {
+            Vector3 diff = transform.position - hit.point;
+            float direction = Mathf.Atan2(diff.z, -diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, direction, 0f);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 delta = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
