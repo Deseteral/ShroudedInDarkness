66edca7f9459282a15c73240f79d524a70519062
Deseteral <deseteral@gmail.com>
2020-04-18 11:54:41 +0200

    Cleanup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,11 @@
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject Sun;
-    public bool DEBUG_DayNightCycle = true;
-
-    private const float DAY_NIGHT_CYCLE_SEC = (10f * 60f);
-
     void Start()
     {
     }
 
     void Update()
     {
-        float timeProgress = DEBUG_DayNightCycle 
-            ? (((Time.timeSinceLevelLoad % DAY_NIGHT_CYCLE_SEC) / DAY_NIGHT_CYCLE_SEC) * 360)
-            : 20f;
-        Sun.transform.rotation = Quaternion.Euler(timeProgress, -30, 0);
     }
 }
