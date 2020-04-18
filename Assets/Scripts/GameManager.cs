d9efdc628eb3b923b2eee10201785a838db3e010
Deseteral <deseteral@gmail.com>
2020-04-18 09:14:21 +0200

    Day night cycle

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Sun;
+
+    private const float DAY_NIGHT_CYCLE_SEC = (10f * 60f);
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float timeProgress = (((Time.timeSinceLevelLoad % DAY_NIGHT_CYCLE_SEC) / DAY_NIGHT_CYCLE_SEC) * 360);
+        Sun.transform.rotation = Quaternion.Euler(timeProgress, -30, 0);
+    }
+}
