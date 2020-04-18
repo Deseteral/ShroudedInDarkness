acb33c9320081473194e5f2f3e524ede21402ee6
Deseteral <deseteral@gmail.com>
2020-04-18 12:33:24 +0200

    Camera follow with simple player controller

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject Target;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Target.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 1000f;
+
+    private new Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 delta = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rigidbody.AddForce(delta * Speed, ForceMode.Force);
+    }
+}
