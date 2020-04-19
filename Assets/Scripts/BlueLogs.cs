bacef34a1ee50c5e3b380e3018fa3c52df5f7312
Deseteral <deseteral@gmail.com>
2020-04-19 15:53:57 +0200

    Blue logs tooltip text

---
+++ Assets/Scripts/BlueLogs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlueLogs : MonoBehaviour
+{
+    private GameObject player;
+    public Text PickUpText;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        // Set text opacity
+        float targetOpacity = (distanceToPlayer < 10f) ? 1f : 0f;
+        float alpha = PickUpText.color.a;
+        alpha += (targetOpacity - alpha) * 0.05f;
+        PickUpText.color = new Color(PickUpText.color.r, PickUpText.color.g, PickUpText.color.b, alpha);
+    }
+}
---
+++ Assets/Scripts/GuiFaceCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GuiFaceCamera : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // TODO: Force UI to ignore Z testing
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+}
