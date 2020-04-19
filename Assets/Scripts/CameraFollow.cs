b330a2f2ef25654cc7f86147fe102d5bf6768005
Deseteral <deseteral@gmail.com>
2020-04-19 11:39:08 +0200

    Fixed the trees

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,9 @@
 public class CameraFollow : MonoBehaviour
 {
     public GameObject Target;
-    private Vector3 offset = new Vector3(-18.37f, 15f, -18.37f); // new Vector3(-6.7f, 11.3f, -11.2f); // new Vector3(-18.37f, 15f, -18.37f);
+    // private Vector3 offset = new Vector3(-18.37f, 15f, -18.37f);  
+    // private Vector3 offset = new Vector3(-6.7f, 11.3f, -11.2f);
+    private Vector3 offset = new Vector3(-37.7f, 32.1f, -37.7f);
 
     void Start()
     {
