bbc2f8f4e2b3a36662b32fc2b1e1fb64ea465727
Deseteral <deseteral@gmail.com>
2020-04-19 12:08:33 +0200

    Ghost AI

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -3,29 +3,66 @@
 public class Ghost : MonoBehaviour
 {
     public float Speed = 1000f;
+    public float TargettingDistance = 20f;
 
     private GameObject player;
+    private GameObject torchlightCollider;
     private new Rigidbody rigidbody;
 
-    private GameObject torchlightCollider;
-
     private Vector3 direction;
+    private Vector3 target;
+    private Vector3 spawnPoint;
+
+    private float confiusionTargetTime = float.MinValue;
+    private bool trackingPlayer = false;
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
         rigidbody = GetComponent<Rigidbody>();
-        torchlightCollider = GameObject.Find("TorchlightCollider");
+        spawnPoint = transform.position;
+
+        player = GameObject.FindWithTag("Player");
+        torchlightCollider = GameObject.Find("Player/TorchlightCollider");
     }
 
     void FixedUpdate()
     {
-        direction = (transform.position - player.transform.position);
+        // AI, set target
+        if (confiusionTargetTime > Time.timeSinceLevelLoad)
+        {
+            target = transform.position;
+        }
+        else
+        {
+            bool prevFrameTrackingPlayer = trackingPlayer;
+
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            if (distanceToPlayer <= TargettingDistance)
+            {
+                target = player.transform.position;
+                trackingPlayer = true;
+            }
+            else
+            {
+                target = spawnPoint;
+                trackingPlayer = false;
+            }
+
+            if (prevFrameTrackingPlayer && !trackingPlayer)
+            {
+                confiusionTargetTime = (Time.timeSinceLevelLoad + Random.Range(2f, 6f));
+            }
+        }
+
+        // Calc direction
+        direction = (transform.position - target);
         direction.y = 0f;
         direction *= -1f;
 
+        // Actually move
         rigidbody.AddForce((direction.normalized * Speed), ForceMode.Force);
 
+        // Rotate towards movement direction
         float directionAngle = Mathf.Atan2(-direction.z, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, directionAngle, 0f);
     }
@@ -34,7 +71,12 @@ public class Ghost : MonoBehaviour
     {
         if (collider.name == "TorchlightCollider" || collider.name == "CampfireTrigger")
         {
-            rigidbody.AddForce((-direction * Speed * 0.5f), ForceMode.Force);
+            BackOff(0.5f);
         }
+    }
+
+    private void BackOff(float scale)
+    {
+        rigidbody.AddForce((-direction * Speed * scale), ForceMode.Force);
     }
 }
