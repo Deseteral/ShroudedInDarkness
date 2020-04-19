15c2ded6ddfff05f62ceacd4bec0d5a7f69fb66f
Deseteral <deseteral@gmail.com>
2020-04-19 16:42:32 +0200

    All wood text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,12 @@ public class GameManager : MonoBehaviour
     private int totalWoodCount = 12;
 
     private GameObject woodCountText;
+    private GameObject allWoodText;
 
     void Start()
     {
         woodCountText = GameObject.Find("GameManager/Canvas/WoodCountText");
+        allWoodText = GameObject.Find("GameManager/Canvas/AllWoodText");
         totalWoodCount = GameObject.FindGameObjectsWithTag("BlueLogs").Length;
     }
 
@@ -21,9 +23,15 @@ public class GameManager : MonoBehaviour
     public void PickUpWood()
     {
         CollectedWood += 1;
-
-        // Show text
         woodCountText.GetComponent<Text>().text = "Collected wood: " + CollectedWood + "/" + totalWoodCount;
-        woodCountText.GetComponent<TextFade>().Show(4f);
+
+        if (CollectedWood == totalWoodCount)
+        {
+            allWoodText.GetComponent<TextFade>().Show(7f);
+        }
+        else
+        {
+            woodCountText.GetComponent<TextFade>().Show(4f);
+        }
     }
 }
