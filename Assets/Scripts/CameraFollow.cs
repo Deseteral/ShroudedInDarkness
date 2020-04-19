3431a496a3d936ee0ac3dd45d810259109316c48
Deseteral <deseteral@gmail.com>
2020-04-19 10:51:25 +0200

    AAAAAAAAaaaaaaa

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@
 public class CameraFollow : MonoBehaviour
 {
     public GameObject Target;
-    private Vector3 offset = new Vector3(-18.37f, 15f, -18.37f);
+    private Vector3 offset = new Vector3(-18.37f, 15f, -18.37f); // new Vector3(-6.7f, 11.3f, -11.2f); // new Vector3(-18.37f, 15f, -18.37f);
 
     void Start()
     {
